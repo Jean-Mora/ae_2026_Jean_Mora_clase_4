c95dab9a025fac9ee4af01b08f69bd858cd09db4
Estudiante LTIC <Estudiante@ltic.local>
2026-05-16 10:32:49 -0500

    Clase 4 1

---
+++ main.kt
@@ -0,0 +1,15 @@
+//  ae_2026_Jean_Mora_clase_4 main.kt
+//estructura del commit clase_4 {{valor incremental}}
+fun main() {
+    // siempre debo tirar mis variable (o constantes)
+    val mensaje: String = "Hola mundo "
+    var variableMutable: Int = 5
+    println(mensaje)
+    println(variableMutable)
+    
+    variableMutable = 10
+    println(variableMutable)
+    
+    mensaje: String = "Hola mundo mutado "
+    println(mensaje)
+}
