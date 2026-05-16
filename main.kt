94b7237395a08e4aae9f80e5ce8abceb5fca8450
Jean-Mora <mora5252@hotmail.com>
2026-05-16 11:22:30 -0500

    Clase 4 5

--- main.kt
+++ main.kt
@@ -1,5 +1,6 @@
 //  ae_2026_Jean_Mora_clase_4 main.kt
 //estructura del commit clase_4 {{valor incremental}}
+
 data class Student(
     val id: Long,
     val name: String,
@@ -8,16 +9,40 @@ data class Student(
     val isActive: Boolean
 )
 
-fun main() {
+fun getStudents(): List<Student> {
     val jorge = Student(
+        id = 1,
         name = "jorge",
         email = "jorge@puce.com",
         grade = 8,
-        id = 1,
         isActive = true
-     
     )
-    
-    val ana = Student(1, "Ana", "ana@puce.com",10,true)
-    println(jorge)
+
+    val pedro = Student(
+        id = 2,
+        name = "pedro",
+        email = "pedro@puce.com",
+        grade = 6,
+        isActive = true
+    )
+
+    val juan = Student(
+        id = 3,
+        name = "juan",
+        email = "juan@puce.com",
+        grade = 9,
+        isActive = true
+    )
+
+    return listOf(jorge, pedro, juan)
+}
+
+fun getResult(grade: Int): String {
+    return if (grade > 7) "Aprobado" else "Reprobado"
+}
+
+fun main() {
+    for (student in getStudents()) {
+        println("${student.name} está ${getResult(student.grade)}")
+    }
 }
